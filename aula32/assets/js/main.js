36ca287fc135cd977740953b059b2dd673eacaeb
levy.buarque <levy.buarque@gmail.com>
2024-08-13 16:48:22 -0300

    aula 33

--- aula32/assets/js/main.js
+++ aula32/assets/js/main.js
@@ -1,1 +1,18 @@
-let a ;
+const pessoa = {
+  // nome: '',
+  // sobrenome: '',
+  idade: 36,
+  endereco: {
+    rua: 'Av Brasil',
+    numero: 320,
+  }
+}
+
+// Atribuição via desestruturação
+const { 
+  nome = 'fulano', //supondo que a variavel não exista, ai seria atribuido um valor padrão
+  sobrenome: s = 'de ciclano', //Pode-se especificar a chave e mudar o nome da variavel
+  idade,
+  ...resto
+} = pessoa;
+console.log(nome, s, idade, resto)
